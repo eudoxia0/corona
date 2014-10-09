752fdfc7a2e87e2a8f80752657dab1aa88ef1c77
Fernando Borretti <eudoxiahp@gmail.com>
2014-10-09 17:46:15 -0200

    Delete, bad idea

--- t/corona.lisp
+++ t/corona.lisp
@@ -8,14 +8,6 @@
 (defparameter +tmp-directory+
   (asdf:system-relative-pathname :corona #p"tmp/"))
 
-(defparameter +test-tar-url+
-  "http://ftp5.usa.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-2.1.1p4.tar.gz"
-  "To test that downloading files and extracting archives works, we use an
-OpenSSH tarball. This URL should exist for the foreseeable future.")
-
-(defparameter +test-tar-pathname+
-  (merge-pathnames #p"test.tar.gz" +tmp-directory+))
-
 ;;; Test suites
 
 (def-suite corona)
@@ -26,19 +18,6 @@ OpenSSH tarball. This URL should exist for the foreseeable future.")
    #+corona-testing t #-corona-testing nil)
   (is (equal corona.files:+corona-directory+
              (asdf:system-relative-pathname :corona #p"t/corona-files/"))))
-
-(test downloading-files
-  (finishes
-    ;; Create directory
-    (ensure-directories-exist +tmp-directory+))
-  (is-true
-   (corona.files:download +test-tar-url+ +test-tar-pathname+))
-  (is-false
-   (corona.files:download +test-tar-url+ +test-tar-pathname+))
-  (is-true
-   (delete-file +test-tar-pathname+))
-  (is-false
-   (probe-file +test-tar-pathname+)))
 
 (run! 'corona)
 
