b3ad508727305f69c4b0384e3aacdde6f0a20091
Fernando Borretti <eudoxiahp@gmail.com>
2014-10-09 17:54:18 -0200

    Update system list

--- src/system-list.lisp
+++ src/system-list.lisp
@@ -8,65 +8,65 @@
   :name :ubuntu
   :version :14.04
   :arch :64
-  :cloud-box (define-box
-               :name "ubuntu-14.04"
-               :author "chef"))
+  :box (define-box
+           :name "ubuntu-14.04"
+           :author "chef"))
 
 (define-system
   :name :ubuntu
   :version :14.04
   :arch :32
-  :cloud-box (define-box
-               :name "ubuntu-14.04-i386"
-               :author "chef"))
+  :box (define-box
+           :name "ubuntu-14.04-i386"
+           :author "chef"))
 
 (define-system
   :name :ubuntu
   :version :13.10
   :arch :64
-  :cloud-box (define-box
-               :name "ubuntu-13.10"
-               :author "chef"))
+  :box (define-box
+           :name "ubuntu-13.10"
+           :author "chef"))
 
 (define-system
   :name :ubuntu
   :version :13.10
   :arch :32
-  :cloud-box (define-box
-               :name "ubuntu-13.10-i386"
-               :author "chef"))
+  :box (define-box
+           :name "ubuntu-13.10-i386"
+           :author "chef"))
 
 (define-system
   :name :ubuntu
   :version :13.04
   :arch :64
-  :cloud-box (define-box
-               :name "ubuntu-13.04"
-               :author "chef"))
+  :box (define-box
+           :name "ubuntu-13.04"
+           :author "chef"))
 
 (define-system
   :name :ubuntu
   :version :13.04
   :arch :32
-  :cloud-box (define-box
-               :name "ubuntu-13.04-i386"
-               :author "chef"))
+  :box (define-box
+           :name "ubuntu-13.04-i386"
+           :author "chef"))
 
 (define-system
   :name :ubuntu
   :version :10.04
   :arch :64
-  :cloud-box (define-box
-               :name "ubuntu-10.04"
-               :author "chef"))
+  :box (define-box
+           :name "ubuntu-10.04"
+           :author "chef"))
 
 (define-system
   :name :ubuntu
   :version :10.04
   :arch :32
-  :cloud-box (define-box
-               :name "ubuntu-10.04-i386"
-               :author "chef"))
+  :box (define-box
+           :name "ubuntu-10.04-i386"
+           :author "chef"))
 
 ;;; Debian
 
@@ -74,33 +74,33 @@
   :name :debian
   :version :7.6
   :arch :64
-  :cloud-box (define-box
-               :name "debian-7.6"
-               :author "chef"))
+  :box (define-box
+           :name "debian-7.6"
+           :author "chef"))
 
 (define-system
   :name :debian
   :version :7.6
   :arch :32
-  :cloud-box (define-box
-               :name "debian-7.6-i386"
-               :author "chef"))
+  :box (define-box
+           :name "debian-7.6-i386"
+           :author "chef"))
 
 (define-system
   :name :debian
   :version :7.4
   :arch :64
-  :cloud-box (define-box
-               :name "debian-7.4"
-               :author "chef"))
+  :box (define-box
+           :name "debian-7.4"
+           :author "chef"))
 
 (define-system
   :name :debian
   :version :7.4
   :arch :32
-  :cloud-box (define-box
-               :name "debian-7.4-i386"
-               :author "chef"))
+  :box (define-box
+           :name "debian-7.4-i386"
+           :author "chef"))
 
 ;;; CentOS
 
@@ -108,7 +108,7 @@
   :name :centos
   :version :6.5
   :arch :64
-  :cloud-box (define-box
+  :box (define-box
                :name "centos-6.5"
                :author "chef"))
 
@@ -116,9 +116,9 @@
   :name :centos
   :version :6.5
   :arch :32
-  :cloud-box (define-box
-               :name "centos-6.5-i386"
-               :author "chef"))
+  :box (define-box
+           :name "centos-6.5-i386"
+           :author "chef"))
 
 ;;; Fedora
 
@@ -126,25 +126,25 @@
   :name :fedora
   :version :20
   :arch :64
-  :cloud-box (define-box
-               :name "fedora-20"
-               :author "chef"))
+  :box (define-box
+           :name "fedora-20"
+           :author "chef"))
 
 (define-system
   :name :fedora
   :version :20
   :arch :32
-  :cloud-box (define-box
-               :name "fedora-20-i386"
-               :author "chef"))
+  :box (define-box
+           :name "fedora-20-i386"
+           :author "chef"))
 
 (define-system
   :name :fedora
   :version :19
   :arch :64
-  :cloud-box (define-box
-               :name "fedora-19"
-               :author "chef"))
+  :box (define-box
+           :name "fedora-19"
+           :author "chef"))
 
 ;;;; BSD forks
 
@@ -154,25 +154,25 @@
   :name :freebsd
   :version :10.0
   :arch :64
-  :cloud-box (define-box
-               :name "freebsd-10.0"
-               :author "chef"))
+  :box (define-box
+           :name "freebsd-10.0"
+           :author "chef"))
 
 (define-system
   :name :freebsd
   :version :9.2
   :arch :64
-  :cloud-box (define-box
-               :name "freebsd-9.2"
-               :author "chef"))
+  :box (define-box
+           :name "freebsd-9.2"
+           :author "chef"))
 
 (define-system
   :name :freebsd
   :version :9.2
   :arch :32
-  :cloud-box (define-box
-               :name "freebsd-9.2-i386"
-               :author "chef"))
+  :box (define-box
+           :name "freebsd-9.2-i386"
+           :author "chef"))
 
 ;;; OpenBSD
 
@@ -180,6 +180,6 @@
   :name :openbsd
   :version :5.5
   :arch :64
-  :cloud-box (define-box
-               :name "openbsd-5.5"
-               :author "tmatilai"))
+  :box (define-box
+           :name "openbsd-5.5"
+           :author "tmatilai"))
