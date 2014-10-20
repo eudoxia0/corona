2a627c7994e7719f4459f6b3ad0da042507e4f80
Fernando Borretti <eudoxiahp@gmail.com>
2014-10-20 19:07:04 -0200

    Update web stuff

--- web/files.lisp
+++ web/files.lisp
@@ -3,35 +3,10 @@
   (:use :cl))
 (in-package :corona-web.files)
 
-(defparameter +style+
-  (lass:compile-and-write
-   `(body
-     :font-family "Source Sans Pro"
-     :width "60%"
-     :margin "0 auto"
-     :color "#222")
-   `(header
-     :margin-bottom "20px"
-     :overflow "auto"
-     (img
-      :width "150px"
-      :float "left"
-      :padding-right "20px")
-     (.title
-      :padding-top "75px"
-      :font-size "3.5em")
-     (.tagline
-      :padding-top "10px"))
-   `(h1
-     :margin-bottom "0")
-   `(hr
-     :margin "0"
-     :border "1px solid #f4c142")))
-
 (defparameter +index+
   (asdf:system-relative-pathname :corona #p"index.html"))
 (defparameter +stylesheet+
-  (asdf:system-relative-pathname :corona #p"web/style.css"))
+  (asdf:system-relative-pathname :corona #p"web/style.lass"))
 
 (defun write-file (pathname content)
   (with-open-file (stream pathname
@@ -41,4 +16,4 @@
     (write-string content stream)))
 
 (write-file +index+ (corona-web.tmpl:index))
-(write-file +stylesheet+ +style+)
+(lass:generate +stylesheet+)
--- web/templates.lisp
+++ web/templates.lisp
@@ -33,15 +33,16 @@
      ,@content
      (:script :src "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"
               "")
-     (:script :src "web/scripts.js"
-              "")
+     (:script :src "web/scripts.js" "")
+     (:script :src "web/highlight-lisp/highlight-lisp.js" "")
+     (:link :rel "stylesheet" :href "web/highlight-lisp/themes/github.css")
      (raw (footer)))))
 
 (defun description ()
   (markup
+   (:h1 "What is this?")
+   (:hr)
    (:section :id "desc"
-     (:h1 "What is this?")
-     (:hr)
      (:p
       "Corona is a library for building and controlling virtual machines. It's
       essentially a clone of "
@@ -58,24 +59,25 @@
   (markup
    (:h1 "Use Cases")
    (:hr)
-   (:h2 "Development Environments")
-   (:p "Corona can be used to create isolated, reproducible development
+   (:section :id "use-cases"
+     (:h2 "Development Environments")
+     (:p "Corona can be used to create isolated, reproducible development
 environments so you and your team can work on the same system.")
-   (:p "No more 'works on my machine', no more difference between development
+     (:p "No more 'works on my machine', no more difference between development
 and production.")
-   (:h2 "Testing")
-   (:p "If you have a library that uses an external tool, like a database server
-or something equally large, you can use Corona to set up a virtual machine and
-install whatever dependencies you need, so the user doesn't actually have to run
-anything on their computer.")
-   (:p "Additionally, since you can set up multiple virtual machines with
+     (:h2 "Testing")
+     (:p "If you have a library that uses an external tool, like a database
+server or something equally large, you can use Corona to set up a virtual
+machine and install whatever dependencies you need, so the user doesn't actually
+have to run anything on their computer.")
+     (:p "Additionally, since you can set up multiple virtual machines with
 different systems, you can use Corona to ensure your library works on most
 operating systems. This is especially useful for testing compilers and similar
 applications where portability is critical.")
-   (:h2 "Building")
-   (:p "You can use Corona as a build server: Fire up virtual machines of the
+     (:h2 "Building")
+     (:p "You can use Corona as a build server: Fire up virtual machines of the
 operating system you want to build on, set them up with everything you need, and
-run the builds.")))
+run the builds."))))
 
 (defun humanize-system-name (name)
   (cond
@@ -90,49 +92,45 @@ run the builds.")))
   (markup
    (:h1 "Available Systems")
    (:hr)
-   (:div :class "row"
-     (:div :class "col-md-6"
-       (:div :id "system-list"
-             :class "list-group"
-         (loop for sys in corona.sys::*known-systems* collecting
-           (let ((name (string-downcase (symbol-name (corona.sys:name sys))))
-                 (version (corona.sys:version sys))
-                 (arch (corona.sys:arch sys)))
-             (markup
-              (:a :href "#machine-def-header" :class "list-group-item"
-                  :data-sys-name name
-                  :data-sys-version version
-                  :data-sys-arch arch
-                  (format nil "~A, ~A, ~A-bit"
-                          (humanize-system-name name)
-                          version
-                          arch)))))))
-     (:div :class "col-md-6"
-       (:h2 :id "machine-def-header"
-            "Machine Definition")
-       (:pre
-        (:code :id "machine-definition"
-         "(defmachine my-machine)"))))))
+   (:section :id "systems"
+     (:ul :id "system-list"
+       (loop for sys in corona.sys::*known-systems* collecting
+         (let ((name (string-downcase (symbol-name (corona.sys:name sys))))
+               (version (corona.sys:version sys))
+               (arch (corona.sys:arch sys)))
+           (markup
+            (:li
+             :data-sys-name name
+             :data-sys-version version
+             :data-sys-arch arch
+             (:a :href "#machine-def-header"
+                 (format nil "~A, ~A, ~A-bit"
+                         (humanize-system-name name)
+                         version
+                         arch)))))))
+     (:div :id "machine-definition"
+           (:h2 :id "machine-def-header"
+                "Machine Definition")
+           (:pre
+            (:code :class "lisp"
+                   :id "definition-code"
+                   "(defmachine my-machine)"))))))
 
 (defun faq ()
   (markup
    (:h1 "FAQ")
    (:hr)
-   (:h2 "Why not Vagrant?")
-   (:p "The advantage over Vagrant is simply that it's written in Common Lisp
+   (:section :id "faq"
+     (:h2 "Why not Vagrant?")
+     (:p "The advantage over Vagrant is simply that it's written in Common Lisp
 and, as such, available in Quicklisp without any external commands. This way,
 the library can be included as a dependency and used without anyone having to
-set up an external tool other than VirtualBox.")))
+set up an external tool other than VirtualBox."))))
 
 (defun index ()
   (layout
    (raw (header))
-   (:div :class "panel panel-default"
-     (:div :class "panel-body"
-       (raw (description))
-       (:section :id "use-cases"
-         (raw (use-cases)))
-       (:section :id "systems"
-         (raw (available-systems)))
-       (:section :id "faq"
-         (raw (faq)))))))
+   (raw (description))
+   (raw (use-cases))
+   (raw (available-systems))
+   (raw (faq))))
