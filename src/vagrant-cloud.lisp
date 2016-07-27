5d185befd33e2539c00630aefaacd621b08e8249
Jack Newton <eternal.turtles@gmail.com>
2016-07-26 22:25:29 -0700

    Update domain: vagrantcloud.com -> atlas.hashicorp.com.

--- src/vagrant-cloud.lisp
+++ src/vagrant-cloud.lisp
@@ -15,7 +15,7 @@
 (in-package :corona.cloud)
 
 (defparameter +vagrant-cloud-url-fmt+
-  "https://vagrantcloud.com/~A/boxes/~A/versions/~A/providers/virtualbox.box"
+  "https://atlas.hashicorp.com/~A/boxes/~A/versions/~A/providers/virtualbox.box"
   "The format control string for a Vagrant Cloud box URL. The first parameter is
   the username, then the box name, and finally the version number.")
 
